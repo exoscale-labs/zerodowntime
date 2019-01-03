6a600b30420108170244f578d9df0319c339db2d
Nicolas Frankel <nicolas.frankel@exoscale.com>
2019-01-03 09:12:16 +0100

    Create hook to automatically create Address entities

--- src/main/kotlin/com/exoscale/boot/zerodowntime/Model.kt
+++ src/main/kotlin/com/exoscale/boot/zerodowntime/Model.kt
@@ -2,6 +2,11 @@ package com.exoscale.boot.zerodowntime
 
 import javax.persistence.*
 import javax.persistence.GenerationType.IDENTITY
+import org.springframework.stereotype.Component
+import org.hibernate.event.service.spi.EventListenerRegistry
+import org.hibernate.event.spi.*
+import org.hibernate.internal.SessionFactoryImpl
+import org.hibernate.persister.entity.EntityPersister
 
 @Entity
 class Person(@Id @GeneratedValue(strategy = IDENTITY) var id: Long = -1,
@@ -18,3 +23,31 @@ class Address(@Id @GeneratedValue(strategy = IDENTITY) var id: Long = -1,
               val city: String,
               val zipCode: String,
               var personId: Long? = null)
+
+val Person.address: Address
+    get() = Address(addressLine1 = addressLine1,
+        addressLine2 = addressLine2,
+        city = city,
+        zipCode = zipCode,
+        personId = id)
+
+@Component
+class WriteAddressListener(emf: EntityManagerFactory) : PostInsertEventListener {
+
+    private val sessionFactory = emf.unwrap(SessionFactoryImpl::class.java)
+
+    init {
+        val registry = sessionFactory.serviceRegistry.getService(EventListenerRegistry::class.java)
+        registry.getEventListenerGroup(EventType.POST_INSERT).appendListener(this)
+    }
+
+    override fun onPostInsert(event: PostInsertEvent) {
+        val entity = event.entity
+        if (entity is Person) {
+            val session = sessionFactory.openSession()
+            session.save(entity.address)
+        }
+    }
+
+    override fun requiresPostCommitHanding(persister: EntityPersister) = false
+}
