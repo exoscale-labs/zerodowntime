368d9ae42bff9f06b102d096ad936f4e4042acf8
Nicolas Frankel <nicolas.frankel@exoscale.com>
2019-01-03 15:07:37 +0100

    Cleanup database and model

--- src/main/kotlin/com/exoscale/boot/zerodowntime/Model.kt
+++ src/main/kotlin/com/exoscale/boot/zerodowntime/Model.kt
@@ -1,9 +1,5 @@
 package com.exoscale.boot.zerodowntime
 
-import org.hibernate.event.service.spi.EventListenerRegistry
-import org.hibernate.event.spi.*
-import org.hibernate.internal.SessionFactoryImpl
-import org.springframework.stereotype.Component
 import javax.persistence.*
 import javax.persistence.GenerationType.IDENTITY
 
@@ -18,11 +14,6 @@ class Person(@Id @GeneratedValue(strategy = IDENTITY) var id: Long = -1,
     init {
         address.person = this
     }
-
-    internal var addressLine1: String? = null
-    internal var addressLine2: String? = null
-    internal var city: String? = null
-    internal var zipCode: String? = null
 }
 
 @Entity
@@ -34,25 +25,4 @@ class Address(@Id @GeneratedValue(strategy = IDENTITY) var id: Long? = null,
     @OneToOne
     @JoinColumn(name = "person_id")
     lateinit var person: Person
-}
-
-@Component
-class WriteAddressListener(emf: EntityManagerFactory) : PreInsertEventListener {
-
-    init {
-        val sessionFactory = emf.unwrap(SessionFactoryImpl::class.java)
-        val registry = sessionFactory.serviceRegistry.getService(EventListenerRegistry::class.java)
-        registry.getEventListenerGroup(EventType.PRE_INSERT).appendListener(this)
-    }
-
-    override fun onPreInsert(event: PreInsertEvent): Boolean {
-        val entity = event.entity
-        if (entity is Address) {
-            entity.person.addressLine1 = entity.addressLine1
-            entity.person.addressLine2 = entity.addressLine2
-            entity.person.city = entity.city
-            entity.person.zipCode = entity.zipCode
-        }
-        return false
-    }
 }
